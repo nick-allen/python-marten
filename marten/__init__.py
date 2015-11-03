5da3928442a884c7b5905ca2b17362ca99fffc2c
Nick Allen <nallen@foregroundsecurity.com>
2015-11-02 20:41:25 -0500

    Return empty Configuration instance when .marten/ directory is missing

--- marten/__init__.py
+++ marten/__init__.py
@@ -4,16 +4,18 @@ from __future__ import absolute_import
 
 import os as _os
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 
 
 # Attempt to auto-load a default configuration from files in <cwd>/.marten/ based on the MARTEN_ENV env variable
 # MARTEN_ENV defaults to 'default'
-config = None
 _marten_dir = _os.path.join(_os.getcwd(), '.marten')
 _os.environ.setdefault('MARTEN_ENV', 'default')
 
 if _os.path.isdir(_marten_dir):
 	from .configurations import parse_directory as _parse_directory
 	config = _parse_directory(_marten_dir, _os.environ['MARTEN_ENV'])
+else:
+	from .configurations import Configuration as _Configuration
+	config = _Configuration({})
